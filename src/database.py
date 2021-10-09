e7676df45f939f97d3a2c71825aba4075fd1a8dd
averagephotographer <averagephotographer@users.noreply.github.com>
2021-10-09 00:54:08 -0500

    reading from csv, added info to example_input

--- src/database.py
+++ src/database.py
@@ -1,8 +1,25 @@
 # database interactions
+import csv
 
 # fn. retrieving data from .csv
     # input: shop name
     # output: none
+csv_file = "example_input.csv"
+fields = []
+rows = []
+
+with open(csv_file, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    fields = next(csvreader)
+    for row in csvreader:
+        rows.append(row)
+    
+    print("Total no. of rows: %d"%(csvreader.line_num))
+
+# print('Field names are:' + ', '.join(field for field in fields))
+
+print(f"{fields=}")
+print(f"{rows=}")
 
 # fn. adding new shops
     # input: name, cost
